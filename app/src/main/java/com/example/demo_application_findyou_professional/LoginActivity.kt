64da65132f936359c0e75d67dc7e3e0763911705
Rajkonch <konchshivam111@gmail.com>
2024-10-23 18:59:26 +0530

    Start simple page login

--- app/src/main/java/com/example/demo_application_findyou_professional/LoginActivity.kt
+++ app/src/main/java/com/example/demo_application_findyou_professional/LoginActivity.kt
@@ -1,27 +1,20 @@
 package com.example.demo_application_findyou_professional
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun singup(view: View) {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
     }
 }
